ee33ce30b26a10fd0f90a906b2c48eb267fbd8a0
Mytchie <97060894+Mytchie@users.noreply.github.com>
2022-01-03 15:55:26 -0500

    Add files via upload

---
+++ Exercise70.py
@@ -0,0 +1,13 @@
+
+
+number_str = input("Enter your number")
+number_int = int(number_str)
+
+result = number_int + 2
+result = result * 3
+result = result - 6
+result = result / 3
+
+print (result)
+
+
